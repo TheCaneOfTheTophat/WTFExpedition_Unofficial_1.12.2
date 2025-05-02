36767a8eddf585ca7f86a7598fb046d2e9d3bfe4
TheCaneOfTheTophat <thecaneofthetophat@gmail.com>
2025-05-02 19:27:47 +0300

    Home scrolls no longer consumed in creative

--- src/main/java/wtf/items/ItemHomeScroll.java
+++ src/main/java/wtf/items/ItemHomeScroll.java
@@ -24,13 +24,16 @@ public class ItemHomeScroll extends Item {
 		if(WTFExpeditionConfig.homeScrollsEnabled) {
 			BlockPos home = player.getBedLocation(world.provider.getDimension());
 			if (home != null) {
-				stack.shrink(1);
+				if(!player.capabilities.isCreativeMode)
+					stack.shrink(1);
+
 				if (!world.isRemote) {
 					player.setPositionAndUpdate(home.getX(), home.getY(), home.getZ());
 					WorldServer worldServer = (WorldServer) world;
 					worldServer.spawnParticle(EnumParticleTypes.PORTAL, home.getX(), home.getY() + random.nextDouble() * 2.0D, home.getZ(), 32, random.nextGaussian(), 0.0D, random.nextGaussian(), 1.0D);
 					worldServer.spawnParticle(EnumParticleTypes.PORTAL, player.posX, player.posY + random.nextDouble() * 2.0D, player.posZ, 32, random.nextGaussian(), 0.0D, random.nextGaussian(), 1.0D);
 				}
+
 				return new ActionResult<>(EnumActionResult.SUCCESS, stack);
 			}
 		}
